99ede5dee94ad9731ffb1cb64ca131205b2039b7
DaRealAqua <42686040+DaRealAqua@users.noreply.github.com>
2021-06-17 21:32:37 +0200

    Update Main.php

--- src/ItemDisplay/Main.php
+++ src/ItemDisplay/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   ____        ____            _    _                     
  |  _ \  __ _|  _ \ ___  __ _| |  / \   __ _ _   _  __ _ 
@@ -10,30 +11,33 @@
 
 namespace ItemDisplay;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\Plugin;
-use pocketmine\utils\TextFormat as C;
-use pocketmine\event\Listener;
-use pocketmine\event\entity\ItemSpawnEvent;
 use pocketmine\Server;
 use pocketmine\item\Item;
 use pocketmine\entity\Entity;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\event\entity\ItemSpawnEvent;
 
 class Main extends PluginBase implements Listener
 {
 
-    public function onEnable() {
+    public function onEnable() 
+    {
 	$this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->notice(TextFormat::GREEN." Enabled");
         // TODO: CONFIG
     }
     
-    public function onDisable() {
+    public function onDisable() 
+    {
         $this->getLogger()->notice(TextFormat::RED." Disabled!");
     }
 
     // TODO: CONFIG
-    public function onSpawn(ItemSpawnEvent $event){
+    public function onSpawn(ItemSpawnEvent $event)
+    {
         $entity = $event->getEntity();
         $item = $entity->getItem();
         $iname = $item->getName();
